9f5adfece6fbdee2b28713c7e4a0433857ac511e
Luis David Carvajal Villalobos <luisdavid.carvajal@ucr.ac.cr>
2026-03-17 08:56:25 -0600

    Display number of CPU cores in HelloWorld program
    Added functionality to display the number of CPU cores.

--- TrabajosEnClase/TrabajoEnClase1/HelloWorld.c
+++ TrabajosEnClase/TrabajoEnClase1/HelloWorld.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
     printf("Hola Mundo\n");
+
+    long num_cores= sysconf(_SC_NPROCESSORS_ONLN);
+    printf("Cores: ", num_cores);
     return 0;
+
+    
 }
