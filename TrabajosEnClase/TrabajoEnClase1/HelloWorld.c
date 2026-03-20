63111347b62b6c12720b2d7f22e13da2b3c4092b
Luis David Carvajal Villalobos <luisdavid.carvajal@ucr.ac.cr>
2026-03-20 07:34:09 -0600

    Add multithreading to HelloWorld program

--- TrabajosEnClase/TrabajoEnClase1/HelloWorld.c
+++ TrabajosEnClase/TrabajoEnClase1/HelloWorld.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 
-int main(void) {
+void* mi_funcion(void* argumento){
     printf("Hola Mundo\n");
-
-    long num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+    return NULL;
+}
+int main(void) {
+    int rc;
+    int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
     printf("Cores: %ld\n", num_cores);
+    pthread_t hilos [num_cores];
+
+    for(int t = 0; t < num_cores; t++) {
+        printf("En main: creando el hilo %ld\n", t);
+        rc = pthread_create(&hilos[t], NULL, mi_funcion, (void*)t);
+    }
+    for(int t = 0; t < num_cores; t++) {
+        pthread_join(hilos[t], NULL);
+    }
     return 0;
 }
