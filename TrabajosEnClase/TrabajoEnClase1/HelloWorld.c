eddf3ab1a7f05cc1b11f81855a3cc2239dcc206d
Luis David Carvajal Villalobos <luisdavid.carvajal@ucr.ac.cr>
2026-03-20 07:44:01 -0600

    Enhance HelloWorld.c with thread ID printing
    Updated thread creation and joining logic to include thread IDs in output.

--- TrabajosEnClase/TrabajoEnClase1/HelloWorld.c
+++ TrabajosEnClase/TrabajoEnClase1/HelloWorld.c
@@ -3,21 +3,32 @@
 #include <pthread.h>
 
 void* mi_funcion(void* argumento){
-    printf("Hola Mundo\n");
+    long id = (long)argumento; 
+    printf("Hola Mundo desde el hilo #%ld\n", id);
     return NULL;
 }
+
 int main(void) {
     int rc;
-    int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
-    printf("Cores: %ld\n", num_cores);
-    pthread_t hilos [num_cores];
+    long num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+    pthread_t hilos[num_cores];
+    long t; // Declarada aquí para evitar errores de C99 y redefinición
 
-    for(int t = 0; t < num_cores; t++) {
+    printf("Cores: %ld\n", num_cores);
+
+    // Primer bucle: Creación
+    for(t = 0; t < num_cores; t++) {
         printf("En main: creando el hilo %ld\n", t);
         rc = pthread_create(&hilos[t], NULL, mi_funcion, (void*)t);
+        if (rc) {
+            printf("Error: %d\n", rc);
+        }
     }
-    for(int t = 0; t < num_cores; t++) {
+
+    // Segundo bucle: Espera
+    for(t = 0; t < num_cores; t++) {
         pthread_join(hilos[t], NULL);
     }
+
     return 0;
 }
