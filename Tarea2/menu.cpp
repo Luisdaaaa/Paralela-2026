23e96dc237b565c60162ec2615eae28257add212
Luisdaaaa <luisdavid.carvajal@ucr.ac.cr>
2026-05-14 11:52:13 -0600

    Esqueleto programa

---
+++ Tarea2/menu.cpp
@@ -0,0 +1,12 @@
+#include "menu.hpp"
+
+void menu::display_menu() {
+    //Menu del programa
+    programa Program;
+    cout << "Menu del programa:" << endl;
+    cout << "1. Calcular serial" << endl;
+    cout << "2. Calcular paralelo" << endl;
+    cout << "3. Ver datos" << endl;
+    cout << "4. Crear archivo Paraview" << endl;
+    cout << "5. Salir" << endl;
+}
---
+++ Tarea2/programa.cpp
@@ -0,0 +1,20 @@
+#include "programa.hpp"
+
+bool programa::Paraview_data(int n, int steps, string filename) {
+    // Implementation for creating Paraview data
+    return true;
+}
+
+bool programa::calculate_steps_serial() {
+    // Implementation for calculating steps in serial
+    return true;
+}
+
+bool programa::calculate_steps_parallel() {
+    // Implementation for calculating steps in parallel
+    return true;
+}
+bool programa::initialize_cube() {
+    // Implementation for initializing the cube
+    return true;
+}
