3e298a9429e718c72eacc6282076d79db455a1c9
Luisdaaaa <luisdavid.carvajal@ucr.ac.cr>
2026-05-14 16:41:35 -0600

    Terminada funcion Paraview_data

--- Tarea2/programa.cpp
+++ Tarea2/programa.cpp
@@ -1,7 +1,38 @@
 #include "programa.hpp"
 
-bool programa::Paraview_data(int n, int steps, string filename) {
+bool programa::Paraview_data(string filename) {
+    int N_TOTAL=N*N*N;
     // Implementation for creating Paraview data
+    filename="./ParaviewData/"+filename+".vtk"; // Usar ruta relativa
+    std::ofstream paraview_archive(filename);
+    if (paraview_archive.is_open()){
+        //Header
+        paraview_archive<<"# vtk DataFile Version 3.0\n";
+        paraview_archive<<"Distribucion de Calor 3D\n";
+        paraview_archive<<"ASCII\n";
+        paraview_archive<<"DATASET STRUCTURED_POINTS\n";
+        paraview_archive<<"DIMENSIONS "<<N<<" "<<N<<" "<<N<<"\n";
+        paraview_archive<<"ORIGIN 0.0 0.0 0.0\n";
+        paraview_archive<<"SPACING 1.0 1.0 1.0\n";
+        paraview_archive<<"POINT_DATA "<<N_TOTAL<<"\n";
+        paraview_archive<<"SCALARS Temperature double\n";
+        paraview_archive<<"LOOKUP_TABLE default\n";
+        
+        //Temperatures
+        for (int k=0; k<N; k++){
+            for(int j=0; j<N; j++){
+                for(int i=0; i<N; i++){
+                    paraview_archive<<cube_old[i][j][k]<<"\n";
+                }
+            }
+        }
+        paraview_archive.close();
+        cout<<"Archivo guardado con exito"<<endl;
+    }
+    else{
+        cout<<"El archivo no se pudo abrir"<<endl;
+        return false;
+    }
     return true;
 }
 
--- Tarea2/programa.hpp
+++ Tarea2/programa.hpp
@@ -1,6 +1,7 @@
 #define N 100
 #define NUM_STEPS 1000
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 using namespace std;
@@ -9,12 +10,12 @@ class programa{
     public:
         bool initialize_cube();
         bool print_cube();
-        bool Paraview_data(int n, int steps, string filename);
+        bool Paraview_data(string filename);
         bool calculate_steps_serial();
         bool calculate_steps_parallel();
         //se debe calcular los tiempos para serial y paralelo
     private:
-        vector<vector<vector<double>>> matrix_old;
-        vector<vector<vector<double>>> matrix_new;
+        vector<vector<vector<double>>> cube_old;
+        vector<vector<vector<double>>> cube_new;
 };
 
