816e97ecfd3b3955a548942c9b3b8f9ea3b060a9
Luisdaaaa <luisdavid.carvajal@ucr.ac.cr>
2026-06-15 17:57:11 -0600

    evolve, initialize_particles, ParticleSimulator

--- Tarea3/particle_sim.hpp
+++ Tarea3/particle_sim.hpp
@@ -1,5 +1,7 @@
 #include "particle.hpp"
 #include <vector>
+#include <cstdlib>
+#include <cmath>
 using namespace std;
 
 class ParticleSimulator {
@@ -12,7 +14,7 @@ class ParticleSimulator {
         bool INI; //Particle static initialization
     public:
         ParticleSimulator(int N, int iterations, bool bandera_imp, bool INI);
-        void initialize_particles();
+        void initialize_particles(int total_processes, int rank);
         bool execute_simulation();
         bool update_Properties();
         bool merge(vector<Particle>& returned_particles, vector<Particle>& local_particles);
