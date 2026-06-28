d8f0a1053f364918961e0d00ce82c409995844f3
jime <jimena.avella@ucr.ac.cr>
2026-06-28 15:04:45 -0600

    Tiempo de ejecucion

--- Tarea3/main.cpp
+++ Tarea3/main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char** argv) {
 
     MPI_Init(&argc, &argv); 
 
+    double start_time = MPI_Wtime();
+    
     int rank , size ;
     
     MPI_Comm_rank ( MPI_COMM_WORLD , & rank ) ;
@@ -21,8 +23,12 @@ int main(int argc, char** argv) {
     int init_flag = std::stoi(argv[4]);
 
     ParticleSimulator sim(num_particles, num_iterations, print_flag, init_flag, rank, size);
+    
     sim.initialize_particles(rank);
     sim.execute_simulation();
+
+    double end_time = MPI_Wtime();
+    std::cout << "Tiempo de ejecucion: " << end_time - start_time << " segundos" << std::endl;
 
     MPI_Finalize();
     return 0;
